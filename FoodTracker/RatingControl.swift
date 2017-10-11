62f271251c57a8f4311fdba119dd25e0e471ca26
Yukari Sakurai <neko3genki@gmail.com>
2017-10-11 13:54:27 +0900

    Create a custom view

---
+++ FoodTracker/RatingControl.swift
@@ -0,0 +1,21 @@
+//
+//  RatingControl.swift
+//  FoodTracker
+//
+//  Created by Sakura on 2017/10/11.
+//  Copyright © 2017年 Sakura. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIStackView {
+
+  //MARK: Initialization
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+  }
+
+  required init(coder: NSCoder) {
+    super.init(coder: coder)
+  }
+}
