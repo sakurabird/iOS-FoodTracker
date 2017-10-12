5b52cb7a2e10e3e10f5acab7884b38114b797e95
Yukari Sakurai <neko3genki@gmail.com>
2017-10-13 02:08:12 +0900

    Add support for Interface Builder

--- FoodTracker/RatingControl.swift
+++ FoodTracker/RatingControl.swift
@@ -8,10 +8,25 @@
 
 import UIKit
 
-class RatingControl: UIStackView {
+// IBDesignableにすると、InterfaceBuilder上でカスタムビューの見た目を確認できたり、プロパティを設定できるようになる
+@IBDesignable class RatingControl: UIStackView {
 
   //MARK: Properties
   private var ratingButtons = [UIButton]()
+
+  // メンバ変数に@IBInspectableをつけると、Interface Builder上で値を設定できる
+  //  var starSize: CGSize = CGSize(width: 44.0, height: 44.0)
+  //  var starCount: Int = 5
+  @IBInspectable var starSize: CGSize = CGSize(width: 44.0, height: 44.0) {
+    didSet {
+      setupButtons()
+    }
+  }
+  @IBInspectable var starCount: Int = 5 {
+    didSet {
+      setupButtons()
+    }
+  }
 
   var rating = 0
 
@@ -32,18 +47,24 @@ class RatingControl: UIStackView {
   }
 
   //MARK: Private Methods
-  private func setupButtons(){
+  private func setupButtons() {
 
-    for _ in 0..<5 {
+    // clear any existing buttons
+    for button in ratingButtons {
+      removeArrangedSubview(button)
+      button.removeFromSuperview()
+    }
+    ratingButtons.removeAll()
 
+    for _ in 0..<starCount {
       // Create the button
-      let button  = UIButton()
+      let button = UIButton()
       button.backgroundColor = UIColor.red
 
       // Add constraints
       button.translatesAutoresizingMaskIntoConstraints = false
-      button.heightAnchor.constraint(equalToConstant: 44.0).isActive = true
-      button.widthAnchor.constraint(equalToConstant: 44.0).isActive = true
+      button.heightAnchor.constraint(equalToConstant: starSize.height).isActive = true
+      button.widthAnchor.constraint(equalToConstant: starSize.width).isActive = true
 
       // Setup the button action
       button.addTarget(self, action: #selector(RatingControl.ratingButtonTapped(button:)), for: .touchUpInside)
@@ -52,6 +73,9 @@ class RatingControl: UIStackView {
       //    自動配置させたい子Viewをコードから追加する場合、addArangedSubview:で行います。
       //    addSubview:では通常通りの子View追加となり、自動配置は行ってくれません。
       addArrangedSubview(button)
+
+      // Add the new button to the rating button array
+      ratingButtons.append(button)
     }
   }
 }
