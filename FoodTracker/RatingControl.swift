bb7e1d7a19cf9f29dc95dd9567f64b9127b82746
Yukari Sakurai <neko3genki@gmail.com>
2017-10-12 23:21:01 +0900

    Add buttons to view

--- FoodTracker/RatingControl.swift
+++ FoodTracker/RatingControl.swift
@@ -10,12 +10,48 @@ import UIKit
 
 class RatingControl: UIStackView {
 
+  //MARK: Properties
+  private var ratingButtons = [UIButton]()
+
+  var rating = 0
+
   //MARK: Initialization
   override init(frame: CGRect) {
     super.init(frame: frame)
+    setupButtons()
   }
 
   required init(coder: NSCoder) {
     super.init(coder: coder)
+    setupButtons()
+  }
+
+  //MARK: Button Action
+  func ratingButtonTapped(button :UIButton) {
+    print("Button Pressed 👍")
+  }
+
+  //MARK: Private Methods
+  private func setupButtons(){
+
+    for _ in 0..<5 {
+
+      // Create the button
+      let button  = UIButton()
+      button.backgroundColor = UIColor.red
+
+      // Add constraints
+      button.translatesAutoresizingMaskIntoConstraints = false
+      button.heightAnchor.constraint(equalToConstant: 44.0).isActive = true
+      button.widthAnchor.constraint(equalToConstant: 44.0).isActive = true
+
+      // Setup the button action
+      button.addTarget(self, action: #selector(RatingControl.ratingButtonTapped(button:)), for: .touchUpInside)
+
+      // Add the button to the stack
+      //    自動配置させたい子Viewをコードから追加する場合、addArangedSubview:で行います。
+      //    addSubview:では通常通りの子View追加となり、自動配置は行ってくれません。
+      addArrangedSubview(button)
+    }
   }
 }
