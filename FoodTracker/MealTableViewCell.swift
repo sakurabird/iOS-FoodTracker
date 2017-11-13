af35f918530ab3aa6b190309149be0a8451cc13c
Yukari Sakurai <neko3genki@gmail.com>
2017-11-13 21:15:01 +0900

    Design custom Table cells

---
+++ FoodTracker/MealTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MealTableViewCell.swift
+//  FoodTracker
+//
+//  Created by Sakura on 2017/11/13.
+//  Copyright © 2017年 Sakura. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
