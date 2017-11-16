91a53f7b34bad70b4b4cc21ea082a80b23935f78
Yukari Sakurai <neko3genki@gmail.com>
2017-11-16 22:16:01 +0900

    Format source code (ctl + i)

--- FoodTracker/MealTableViewController.swift
+++ FoodTracker/MealTableViewController.swift
@@ -15,85 +15,85 @@ class MealTableViewController: UITableViewController {
 
   var meals = [Meal]()
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+  override func viewDidLoad() {
+    super.viewDidLoad()
 
-      // Load the sample data.
-      loadSampleMeals()
+    // Load the sample data.
+    loadSampleMeals()
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+  // MARK: - Table view data source
+
+  override func numberOfSections(in tableView: UITableView) -> Int {
+    return 1
+  }
+
+  override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    // TableViewの行の数
+    return meals.count
+  }
+
+  override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    // 表示されている行に対する設定を記述する
+
+    // Table view cells are reused and should be dequeued using a cell identifier.
+    let cellIdentifier = "MealTableViewCell"
+
+    //      let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
+
+    guard let cell =	tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as? MealTableViewCell else {
+      fatalError("The dequeued cell is not an instance of MealTableViewCell.")
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    // Fetches the appropriate meal for the data source layout.
+    let meal = meals[indexPath.row]
 
-    // MARK: - Table view data source
+    cell.nameLabel.text = meal.name
+    cell.photoImageView.image = meal.photo
+    cell.ratingControl.rating = meal.rating
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
-    }
+    return cell
+  }
 
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // TableViewの行の数
-        return meals.count
-    }
+  /*
+   // Override to support conditional editing of the table view.
+   override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+   // Return false if you do not want the specified item to be editable.
+   return true
+   }
+   */
 
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-      // 表示されている行に対する設定を記述する
+  /*
+   // Override to support editing the table view.
+   override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+   if editingStyle == .delete {
+   // Delete the row from the data source
+   tableView.deleteRows(at: [indexPath], with: .fade)
+   } else if editingStyle == .insert {
+   // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+   }
+   }
+   */
 
-      // Table view cells are reused and should be dequeued using a cell identifier.
-      let cellIdentifier = "MealTableViewCell"
+  /*
+   // Override to support rearranging the table view.
+   override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
 
-//      let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
+   }
+   */
 
-      guard let cell =	tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as? MealTableViewCell else {
-        fatalError("The dequeued cell is not an instance of MealTableViewCell.")
-      }
-
-      // Fetches the appropriate meal for the data source layout.
-      let meal = meals[indexPath.row]
-
-      cell.nameLabel.text = meal.name
-      cell.photoImageView.image = meal.photo
-      cell.ratingControl.rating = meal.rating
-
-      return cell
-    }
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
+  /*
+   // Override to support conditional rearranging of the table view.
+   override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+   // Return false if you do not want the item to be re-orderable.
+   return true
+   }
+   */
 
   //MARK: - Navigation
 
@@ -113,7 +113,7 @@ class MealTableViewController: UITableViewController {
       }
 
       guard let selectedMealCell = sender as? MealTableViewCell else {
-         fatalError("Unexpected sender: \(sender)")
+        fatalError("Unexpected sender: \(sender)")
       }
 
       guard let indexPath = tableView.indexPath(for: selectedMealCell) else {
